d023dbb38a9671714c796dd5f582354ab53ea90a
Zakai Ikar <zakariyeiikar669@gmail.com>
2025-05-16 15:36:42 +0300

    addd

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
-import 'package:carmarketplace/screens/welcome_screen.dart';
-import 'package:carmarketplace/screens/register_screen.dart';
-import 'package:carmarketplace/screens/login_screen.dart';
-import 'package:carmarketplace/screens/authstate.dart'; // optional: for future logic after login
+import 'package:carmarketplace/screens/admin_dashboard.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -25,14 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // Start with welcome screen
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const WelcomeScreen(),
-        '/register': (context) => const RegistrationScreen(),
-        '/login': (context) =>  LoginScreen(),
-        '/auth': (context) => const Authstate(), // optional for future use
-      },
+      home: const AdminDashboard(),
     );
   }
 }
