744dbd69da8d86a35b774ab6174f34b2ee67701d
Abdirashiid bile <abdirashiidbile5@gmail.com>
2025-05-26 16:28:37 +0300

    home screen

--- lib/main.dart
+++ lib/main.dart
@@ -86,7 +86,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const ViewrentalCarPage() // viewStart from the welcome screen
+        home: const HomeScreen()                             
 
         );
   }
